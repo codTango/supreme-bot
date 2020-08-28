089761c2b8ad1a545ccd04d07fb8979a30ac50b0
Connor Tang <connor.tang@oracle.com>
2020-08-27 21:54:52 -0400

    scroll bar & task group title

--- app/components/taskGroup/TaskGroup.tsx
+++ app/components/taskGroup/TaskGroup.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import { IconButton } from '@material-ui/core';
 
 export default function TaskGroup(): JSX.Element {
 
   return (
     <div className="task-group">
       <div className="gradient-box">
+        <div className="window-title task-group-title">
+          <span className="title-text">TASK GROUPS</span>
+          <IconButton size="small">
+            <AddIcon style={{color: '#de2e31'}} />
+          </IconButton>
+        </div>
         <div className="task-area">task list here</div>
       </div>
     </div>
--- app/containers/TaskPage.tsx
+++ app/containers/TaskPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Counter from '../components/counter/Counter';
 import TaskGroup from '../components/taskGroup/TaskGroup';
 
 export default function ProfilePage() {
